5f76d6e492a64d3cec2c2fc0197b1821ed51479a
nathanwmeyer <nathanwm.meyer@gmail.com>
2021-06-21 07:29:41 -0700

    update commit

--- cst-235 benchmark/src/main/java/business/WordSearchBusinessInterface.java
+++ cst-235 benchmark/src/main/java/business/WordSearchBusinessInterface.java
@@ -1,7 +1,10 @@
 package business;
 
+import javax.ejb.Local;
+
 import beans.Bible;
 
+@Local
 public interface WordSearchBusinessInterface {
 
 	public String[] breakDown(String string);
--- cst-235 benchmark/src/main/java/business/WordSearchBusinessService.java
+++ cst-235 benchmark/src/main/java/business/WordSearchBusinessService.java
@@ -12,7 +12,6 @@ import beans.Bible;
 /**
  * Session Bean implementation class WordSearchBusinessService
  */
-@Default
 @Stateless
 @Local
 @LocalBean
